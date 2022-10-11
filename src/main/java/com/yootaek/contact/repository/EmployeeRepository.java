275f059d4d43a93a005b6e4c63945bc0b16f6afa
yootaek <eldbxor@naver.com>
2022-10-11 20:54:10 +0900

    Repository 인터페이스 정의

---
+++ src/main/java/com/yootaek/contact/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.yootaek.contact.repository;
+
+import com.yootaek.contact.domain.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository {
+    List<Employee> findAll();
+    Employee findByName(String name);
+    Employee save(Employee employee);
+}
